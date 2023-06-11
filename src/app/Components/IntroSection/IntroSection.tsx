714c3b46143356997f9ec4ba830f88d703cb67f8
5hy4m <vcsshyam1998@gmail.com>
2023-06-11 11:05:15 +0530

    Tweaked intro section

---
+++ src/app/Components/IntroSection/IntroSection.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import localFont from "next/font/local";
+
+const BoldFont = localFont({
+  src: "../../../../public/fonts/Sequel-Sans-Heavy-Disp.ttf",
+});
+interface Props {}
+
+function IntroSection(props: Props) {
+  const {} = props;
+
+  return (
+    <div className="grid grid-cols-2 container md:mx-auto  px-5 flex justify-center items-center bg-black text-white h-full">
+      <div>
+        <span
+          className={
+            BoldFont.className +
+            " text-sm bg-clip-text text-transparent md:text-lg bg-gradient-to-r from-[var(--primary-gradient-from)] to-[var(--primary-gradient-to)]"
+          }
+        >
+          Welcome to my site.
+        </span>
+
+        <div className={BoldFont.className + " text-5xl md:text-7xl "}>
+          I&apos;m{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[var(--primary-gradient-from)] to-[var(--primary-gradient-to)]">
+            Shyam Kumar
+          </span>
+          , a Fullstack developer.
+        </div>
+
+        <p className="py-4 text-[var(--secondary-color)]">
+          I love writing code that takes things next level creating highly
+          performant websites, automated API integrations, building my own
+          dev-tools, and creating stunning user-experiences that makes you feel
+          WOW!.
+          <br />
+          <br />I am always keen to learn and explore new technologies,
+          frameworks and programming languages. Currently, I&apos;m learning
+          about Astro and Replicache.
+        </p>
+      </div>
+      {/* <NewtonsCradle /> */}
+    </div>
+  );
+}
+
+export default IntroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,9 @@
-import localFont from "next/font/local";
-
-const BoldFont = localFont({
-  src: "../../public/fonts/Sequel-Sans-Heavy-Disp.ttf",
-});
+import { IntroSection } from "./Components/IntroSection";
 
 export default async function Home() {
   return (
-    <main
-      className={"bg-black text-white h-full flex justify-center items-center"}
-    >
-      <div className="md:container md:mx-auto mx-4 md:mx-20 lg:mx-40 ">
-        <span
-          className={
-            BoldFont.className +
-            " text-sm bg-clip-text text-transparent md:text-lg color-red bg-gradient-to-r from-[#ef473a] to-[#cb2d3e]"
-          }
-        >
-          Welcome to my site.
-        </span>
-
-        <div className={BoldFont.className + " text-5xl md:text-7xl "}>
-          I&apos;m{" "}
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[var(--primary-gradient-from)] to-[var(--primary-gradient-to)]">
-            Shyam Kumar
-          </span>
-          , a Fullstack developer.
-        </div>
-
-        <p className="py-4 text-[var(--secondary-color)]">
-          I love writing code that takes things next level creating highly
-          performant websites, automated API integrations, building my own
-          dev-tools, and creating stunning user-experiences that makes you feel
-          WOW!.
-          <br />
-          <br />I am always keen to learn and explore new technologies,
-          frameworks and programming languages. Currently, I&apos;m learning
-          about Astro and Replicache.
-        </p>
-      </div>
+    <main className={"h-full bg-black"}>
+      <IntroSection />
     </main>
   );
 }
